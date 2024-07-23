1f7a6bb0aad088774c3ff2f2ae744d66dae3abd3
ozgurcengiz2008 <ozgurcengiz2008@gmail.com>
2024-07-23 11:40:24 +0300

    minör değişiklikler

--- setup.py
+++ setup.py
@@ -1,28 +1,34 @@
 from cx_Freeze import setup, Executable
+import sys
+import os
 
-executables = [
-    Executable(
-        "main_11.py",
-        base="Win32GUI",
-        # icon="your_icon.ico",
-        target_name="JSP_converter.exe"  # Hedef dosya adını burada belirtin
-    )
+# Increase recursion limit to avoid recursion errors
+sys.setrecursionlimit(5000)
+
+# include necessary packages and modules
+includes = ["os", "sys", "PyQt5.QtCore", "PyQt5.QtGui", "PyQt5.QtWidgets", "mutagen", "pydub", "vlc"]
+excludes = ["tkinter", "unittest"]
+
+# include additional files
+data_files = [
+    ("", ["main2_form_tr.qm", "main2_form_en.qm"]),
+    ("images", ["images/loading-6.gif"]),
+    # Uncomment and correct if necessary
+    # ("indirilen_video_ses_dosyalari", ["indirilen_video_ses_dosyalari"])  # Ensure the path and files are correct
 ]
 
-options = {
-    "build_exe": {
-        "include_msvcr": True,
-        # "includes": ["your_module"],
-        # "excludes": ["unwanted_module"],
-        # "packages": ["your_package"],
-        "include_files": ["images/loading-6.gif"],
-    }
-}
-
+# Setup configuration
 setup(
-    name="JSP_Converter",
-    version="1.0",
-    description="Mp3.ogg.wav.mp4 formatlı seslerin birbirine dönüşümünü sağlar. Video dosyalarından sesleri ayıklar ve dönüştürür.",
-    executables=executables,
-    options=options,
+    name="MusicConverter",
+    version="1.0.1",
+    description="A music converter application",
+    options={
+        "build_exe": {
+            "includes": includes,
+            "excludes": excludes,
+            "include_files": data_files,
+            "build_exe": "dist",
+        }
+    },
+    executables=[Executable("jspconverterv1.0.1.py", base="Win32GUI", icon="icons/jsproductionicon.ico")]
 )
